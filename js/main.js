b4d4add541deb40e1eeaebee614f3c316b921097
cartveroj <cartagenaveronica.19@gmail.com>
2024-02-01 17:07:50 +0100

    ejercicio finalizado 1.02.24

--- js/main.js
+++ js/main.js
@@ -10,20 +10,30 @@
 
 import { Pilota } from "./pilota.js";
 
-
+//variables globales
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-console.log(height +"col"+ width)
+let arrayPilotas = [];
+//cambiamos el fondo de la pantalla a negro
+canvas.setAttribute("style", "background-color: black;");
 
-let Pilota1 = new Pilota(50, 100, 4, 4, "blue", 10);
-console.log(Pilota1);
-Pilota1.dibuixa(ctx);
-//Pilota1.mou(width, height);
+//creamos las pelotas 
+crearPelotas();
+
+//Funcion que se encarga de crear las pelotas
+function crearPelotas(){
+  for(let i=0 ; i<25; i++){
+    let mida = random(10,20);
+    let x = random((0+mida),(width-mida))
+    let y = random((0+mida),(height-mida))
+    let pilota= new Pilota(x,y,random(-3,6),random(-3,5),randomRGB(),mida);
+    arrayPilotas.push(pilota);
+  }
+}
 
 // funció per generar un número aleatori entre dues xifres
 
@@ -38,21 +48,40 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-requestAnimationFrame(loop)
-function loop(){
-  let arrayPilotas = [];
-  const canvas = document.querySelector('canvas');
-  canvas.setAttribute("style", "background-color: black;");
-  for(let i=0 ; i<25; i++){
-    let mida = random(10,20);
-    let x = random((0+mida),(width-mida))
-    let y = random((0+mida),(height-mida))
-    let pilota= new Pilota(x,y,0,0,randomRGB(),mida);
-    arrayPilotas.push(pilota);
+
+//funcion que se encarga de detectar la colision entre los objetos
+//recorremos los dos ejes X y Y para detectar si colisionan o no
+function detectarColision() {
+  for (let i = 0; i < arrayPilotas.length; i++) {
+    const pA = arrayPilotas[i];
+    for (let j = i + 1; j < arrayPilotas.length; j++) {
+      const pB = arrayPilotas[j];
+      if (colision(pA, pB)) {
+        pA.color = randomRGB();
+        pB.color = randomRGB();
+      }
+    }
   }
-  console.log(arrayPilotas);
-  arrayPilotas.forEach((element)=>{
-    element.dibuixa(ctx);
-    element.mou(width,height);
-  })
 }
+//funcion que retorna un boolean que se encarga de verificar si colisionan dos objetos
+function colision(pA,pB){
+  let distancia = Math.hypot(Math.abs(pA.x - pB.x),Math.abs(pA.y - pB.y));
+  return (pA.mida + pB.mida) >= distancia;
+}
+
+/*Funcion que dibuja y mueve a las pelotas */
+function loop(){
+  ctx.clearRect(0,0,width,height); //Borra los píxeles especificados dentro de  el rectángulo dado
+  arrayPilotas.forEach((pilota)=>{
+    
+    pilota.dibuixa(ctx);
+    pilota.mou(width,height);
+
+  })
+  detectarColision();
+  window.requestAnimationFrame(loop);
+
+}
+/*requestAnimationFrame es una función que solicita al navegador 
+que llame a la funcion loop antes de que se actualice el siguiente fotograma. */
+window.requestAnimationFrame(loop);
--- js/pilota.js
+++ js/pilota.js
@@ -1,3 +1,4 @@
+/*Clase pilota compuesta por sus propiedades y metodos */
 export class Pilota{
     constructor(x, y, velX, velY, color, mida) {
       this.x=x;
@@ -12,20 +13,22 @@ export class Pilota{
         ctx.fillStyle = this.color; //Color amb que dibuixarem
         ctx.arc(this.x, this.y, this.mida, 0, 2 * Math.PI); //Dibuix d’un arc
         ctx.fill(); // Finalitza el dibuix i l’omple amb el color ja esmenat
+
     }
-    mou(width , height){
-
-        if((this.x + this.mida) > width){
-            this.velX = 0 - this.velX;
-        }else if((this.x - this.mida) < 0){
-            this.velX = 0 - this.velX;
-        }else if((this.y + this.mida) > height){
-            this.velY = 0 - this.velY;
-        }else if((this.y - this.mida) < 0){
-            this.velY = 0 - this.velY;
+    /*metodo que se encarga de modificar la posicio del objeto */
+    mou(width, height) {
+        if ((this.x + this.mida) > width) {
+          this.velX = 0 - this.velX;
+        } else if ((this.x - this.mida) < 0) {
+          this.velX = 0 - this.velX;
+        } else if ((this.y + this.mida) > height) {
+          this.velY = 0 - this.velY;
+        } else if ((this.y - this.mida) < 0) {
+          this.velY = 0 - this.velY;
         }
-
+      
+        // Actualiza la posición del objeto
         this.x = this.x + this.velX;
         this.y = this.y + this.velY;
-    }
+      }
   }
